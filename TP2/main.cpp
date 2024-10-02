0aba82fc1eec43e4e2ce9a6b4f9bd151d624da85
Vasilis Skarleas <51437323+vskarleas@users.noreply.github.com>
2024-10-02 13:03:12 +0200

    Added Makefile and standar librarues

---
+++ TP2/main.cpp
@@ -0,0 +1,17 @@
+/** Bibliothèques C++ intégrées */
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+/** Bibliothèques OpenCV intégrées */
+#include "opencv2/highgui.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+/** Bibliothèques C intégrées */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>
